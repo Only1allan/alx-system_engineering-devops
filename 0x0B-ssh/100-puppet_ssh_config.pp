468e7fda1099acfc7a453c54e5757d7cef8dfde6
Only1allan <kariukiallan850@gmail.com>
2023-09-24 23:23:36 +0300

    made config changes using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+file { '/etc/ssh/ssh_config':
+    ensure => present,
+    mode   => '0600',
+    owner  => 'dev254',
+    group  => 'dev254',
+    content => "    
+        Host remote-server
+            Hostname 100.25.134.21
+            User ubuntu
+            IdentityFile /home/.ssh/.ssh/school
+            PasswordAuthentication no
+    ",
+}
