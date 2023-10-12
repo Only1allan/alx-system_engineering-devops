b1615f295b8f9a213e9cc870ba63d56744f82b9b
Only1allan <kariukiallan850@gmail.com>
2023-10-12 05:03:52 +0300

    fixed errors

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,40 +1,27 @@
 #puppet script that configures a new Ubuntu machine to 
 
-# Install Nginx
+exec {'update_system':
+  command => '/usr/bin/apt-get -y update',
+}
+
 package { 'nginx':
-    ensure => installed,
+  ensure => 'installed',
 }
 
-# Set up default Nginx page
-file { '/usr/share/nginx/html/index.nginx-debian.html':
-    content => 'Hello World!',
-}
-
-# Configure custom 404 page
-file { '/usr/share/nginx/html/404.html':
-    content => 'Ceci n\'est pas une page',
-}
-
-# Configure redirection page
-file_line { 'redirect':
-    path => '/etc/nginx/sites-available/default',
-    line => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGU1wu4/ permanent;',
-}
-
-# Configure custom header
-file_line { 'custom_header':
-    path => '/etc/nginx/sites-available/default',
-    line => 'add_header X-Served-By $HOSTNAME;',
-}
-
-# Restart Nginx
 service { 'nginx':
-    ensure => running,
-    enable => true,
-    require => [
-        File['/usr/share/nginx/html/index.nginx-debian.html'],
-        File['/usr/share/nginx/html/404.html'],
-        File_line['redirect'],
-        File_line['custom_header'],
-    ],
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html' :
+  content => 'Hello World!',
+}
+
+file_line { 'Creating header':
+  	ensure => present,
+  	path   => '/etc/nginx/sites-available/default',
+  	line   => "\tadd_header X-Served-By ${hostname};",
+  	after  => 'server_name _;',
+	require => Service['nginx'],
 }
