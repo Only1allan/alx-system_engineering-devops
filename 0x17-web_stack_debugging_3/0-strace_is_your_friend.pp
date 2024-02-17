c6234c7c97d58d1bf7366449d9e85557a9cad159
Allan Kariuki <90037613+Only1allan@users.noreply.github.com>
2024-02-17 17:49:54 +0300

    fix apache error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,13 @@
-// correct file path for apache2
+# correct file path for apache2
+
 file { '/var/www/html/wp-settings.php':
-  ensure => file,
+    ensure => file,
 }
 
 exec { 'find-class':
-  command => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-  path    => '/bin:/usr/bin',
-  require => File['/var/www/html/wp-settings.php'],
+    command => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+    path    => '/bin:/usr/bin',
+    require => File['/var/www/html/wp-settings.php'],
 }
 
 service { 'apache2':
