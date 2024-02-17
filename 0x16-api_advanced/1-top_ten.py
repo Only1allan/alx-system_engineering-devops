9e1d3feff73d44439ff7bdc0522b2818e337c7c0
Allan Kariuki <90037613+Only1allan@users.noreply.github.com>
2024-02-17 17:31:23 +0300

    more fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,15 +8,13 @@ import requests
 def top_ten(subreddit):
     """Function that queries the Reddit API and prints the titles of the
       first 10 hot posts listed for a given subreddit."""
-    user = {'User-Agent': 'Mozilla/5.0'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    results = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
+        headers={'User-Agent': 'Mozilla/5.0'})
+    if results.status_code == 200:
+        response_data = results.json()
+        titles = response_data['data']['children']
+        for title in titles:
+            print(title['data']['title'])
+    else:
         print(None)
-
-
-if __name__ == "__main__":
-    top_ten(sys.argv[1])
