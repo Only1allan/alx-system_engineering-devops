db4c2d9d039d2d0495d929f5ca5adf148c35a88f
Allan Kariuki <90037613+Only1allan@users.noreply.github.com>
2024-03-05 21:07:13 +0300

    fix import issues

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Module for number of subscribers"""
+import requests
 
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
-    import requests
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
