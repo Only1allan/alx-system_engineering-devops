0ab0f8a9f57534c23bf32c13faed216288f0d2d6
Allan Kariuki <90037613+Only1allan@users.noreply.github.com>
2024-03-05 21:38:02 +0300

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Module for number of subscribers"""
-import sys
 import requests
 
 
@@ -8,14 +7,10 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'Custom'}
     response = requests.get(url, headers=headers,
                             allow_redirects=False, timeout=5)
 
     if response.status_code != 200:
         return 0
     return response.json().get('data').get('subscribers')
-
-
-if __name__ == 'main':
-    number_of_subscribers(sys.argv[1])
