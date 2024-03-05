a417fe1046df3b4e577a9aee84fd9246bcc15e9f
Allan Kariuki <90037613+Only1allan@users.noreply.github.com>
2024-03-05 21:44:01 +0300

    remove allow redirects false

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,8 +12,7 @@ def number_of_subscribers(subreddit):
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Custom'}
-    response = requests.get(url, headers=headers,
-                            allow_redirects=False, timeout=5)
+    response = requests.get(url, headers=headers,timeout=5)
 
     if response.status_code != 200:
         return 0
