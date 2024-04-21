bbd4409290085c6ffb7f3ea98795332b671b5c8e
Allan Kariuki <kariukiallan850@gmail.com>
2024-04-21 03:35:31 +0300

    error fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,12 @@ subscribers module
 import requests
 
 
-def number_of_subscribers(subbreddit):
+def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
 
     header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     url = "https://www.reddit.com"
-    query = "/r/{}/about.json".format(subbreddit)
+    query = "/r/{}/about.json".format(subreddit)
     response = requests.get(url + query, headers=header, allow_redirects=False)
 
     if response.status_code == 200:
