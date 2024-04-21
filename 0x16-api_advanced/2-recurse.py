40456d8d09eaedcf940abcdba3f7fd28fd26ef0b
Allan Kariuki <kariukiallan850@gmail.com>
2024-04-21 03:52:24 +0300

    lint fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,8 @@ import requests
 def recurse(subreddit, hot_list=[], after=None):
     """Returns a list of all hot posts for a given subreddit"""
     header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)", "Accept": "application/json"}
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "Accept": "application/json"}
     url = "https://www.reddit.com"
     query = "/r/{}/hot.json".format(subreddit)
     response = requests.get(url + query, headers=header,
