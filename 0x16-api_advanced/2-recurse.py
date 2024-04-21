39fb15690bf002e93b6a683da69050496b640141
Allan Kariuki <kariukiallan850@gmail.com>
2024-04-21 03:50:20 +0300

    recursuvely find posts

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+recursing module
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Returns a list of all hot posts for a given subreddit"""
+    header = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)", "Accept": "application/json"}
+    url = "https://www.reddit.com"
+    query = "/r/{}/hot.json".format(subreddit)
+    response = requests.get(url + query, headers=header,
+                            params={"after": after}, allow_redirects=False)
+
+    if response.status_code == 200:
+        for post in response.json()['data']['children']:
+            hot_list.append(post['data']['title'])
+        after = response.json()['data']['after']
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        return hot_list
+    else:
+        return None
